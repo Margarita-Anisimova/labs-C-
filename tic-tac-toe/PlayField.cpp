a18b7c33b019c0496f2937138feb4f353269d0ea
Margarita-Anisimova <margarita311@autorambler.ru>
2020-04-19 12:55:38 +0500

    Исправление ошибок 19.04.00
    Устранение недочетов предыдущего коммита

--- tic-tac-toe/PlayField.cpp
+++ tic-tac-toe/PlayField.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-PlayField::CellIdx::CellIdx(int i) 
+PlayField::CellIdx::CellIdx(int i)
 {
 	x = i / size;
 	y = i % size;
@@ -22,7 +22,7 @@ vector<PlayField::CellIdx>  PlayField::getEmptyCells() const
 	for (int i = 0; i < size * size; ++i)
 		if (field[i] == csEmpty)
 		{
-			CellIdx cell(i);
+			PlayField::CellIdx cell(i);
 			emptyCells.push_back(cell);
 		}
 	return emptyCells;
@@ -41,7 +41,7 @@ PlayField PlayField::operator+(CellIdx cell)
 	return *this;
 }
 
-tuple<int , int> PlayField::GetCountCrossNought() const
+tuple<int, int> PlayField::GetCountCrossNought() const
 {
 	int countCross = 0;
 	int countNought = 0;
@@ -56,7 +56,7 @@ tuple<int , int> PlayField::GetCountCrossNought() const
 			break;
 		default:
 			break;
-		}	
+		}
 	return make_tuple(countCross, countNought);
 }
 
@@ -66,13 +66,12 @@ PlayField::State PlayField::get_nextMove() const
 	return (get<0>(count) > get<1>(count)) ? csNought : csCross;
 }
 
-void PlayField::checkLine(int start, int end, int step, int& countWin, State& winState) const
+void PlayField::checkLine(int start, int end, int step, State& winState) const
 {
 	for (int i = start; i <= end; i = i + step)
 	{
 		if (i == end)
 		{
-			countWin++;
 			winState = field[i];
 			break;
 		}
@@ -82,7 +81,7 @@ void PlayField::checkLine(int start, int end, int step, int& countWin, State& wi
 }
 
 PlayField::GameState PlayField::checkFieldStatus() const
-{	
+{
 	int countWin = 0;
 	State winState = csEmpty;
 	checkLine(0, size * size - 1, size + 1, winState);
@@ -98,7 +97,7 @@ PlayField::GameState PlayField::checkFieldStatus() const
 	}
 
 	auto count = GetCountCrossNought();
-	if (abs(get<0>(count) - get<1>(count)) > 1)
+	if (abs(get<0>(count) - get<1>(count)) > 1 && countWin > 1)
 		return fsInvalid;
 	switch (winState)
 	{
